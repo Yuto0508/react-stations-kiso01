d397d195b78c821ede33c79d71db3ffe28a6963c
Yuto0508 <yuto4139@outlook.jp>
2023-11-01 13:41:47 +0900

    main

--- src/ThreadPostList.jsx
+++ src/ThreadPostList.jsx
@@ -1,3 +1,37 @@
-export const ThreadPostList = () => {
-  
-}
+import React, { useState, useEffect } from "react";
+
+export const PostList = ({ threadId }) => {
+  const [posts, setPosts] = useState([]);
+
+  async function getPosts() {
+    const url = `https://railway.bulletinboard.techtrain.dev/threads/${threadId}/posts`;
+
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    if (response.ok) {
+      const postData = await response.json();
+      setPosts(postData);
+    } else {
+      console.log("APIリクエストが失敗しました");
+    }
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [threadId]);
+
+  return (
+    <div className="postList">
+      <h2>投稿一覧</h2>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <strong>{post.author}:</strong> {post.content}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
